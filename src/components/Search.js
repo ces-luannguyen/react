d326fc6278199541d29dc4d5f18584102f415c65
Luan Nguyen <luan.nguyen@codeenginestudio.com>
2022-07-04 16:20:13 +0700

    useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,78 +9,29 @@ const Search = () => {
     const [state1, setState1] = useState('')
     const [state2, setState2] = useState(2);
 
-
-
-    let var1 = 0;
-
-    const increment = ()=>{
-        return ++var1;
+    const event1 = ()=>{
+        console.log('event1');
     }
 
-    console.log(increment());
-
-
-    const num1= 5;
-
-    const num2=9; // ghi de num2 global ben ngoai
-
-    const comp1 = <div>Hello world</div>
-
-
-    const renderluckhoitao = () =>{
-        console.log('Render init');
+    const event2 = () =>{
+        setState2(state2+1);
     }
 
-    const rendermoilanrender = ()=>{
-        console.log('Moi lan render moi lan chay');
-    }
-
-    const rendermoilanthaydoidulieu = () =>{
-        console.log('co thay doi state2');
-    }
-
-    // 
     useEffect(()=>{
-        rendermoilanthaydoidulieu();
+        event1();
 
         return (
-            ()=>{
-                console.log('return;')
+            () =>{
+                console.log('return');
             }
         )
-    }, [state2]);
-
-    const event1 = ()=>{
-        alert('Hello');
-        setState1('Luan Nguyen');
-        setState2(11);
-    }
-
-    const event2 = ()=>{
-        alert('Event2');
-
-        const function1 = () =>{
-            alert('Function1');
-        }
-
-        function1();
-    }
-
-
-    console.log('Hello moi lan render');
-
-    // const event3 = ()=>{
-    //     function1();
-    // }
+    },[])
 
     return (
-        <div>Search {num1+num2}
-        {comp1}
-        <br />
+        <div>
         <button onClick={event1}>Event1</button>
-        {state1} {state2}
-        <br />
         <button onClick={event2}>Event2</button>
+        
 
         </div>
     )
